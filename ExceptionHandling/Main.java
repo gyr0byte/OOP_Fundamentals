9f58b0ed9aa00d4d15192156fdc28d34307354f9
Gauravlovescode <gqurav69@gmail.com>
2026-01-05 21:25:09 +0545

    Refactor Main class to use try-with-resources for Scanner initialization and improve exception handling structure

--- ExceptionHandling/Main.java
+++ ExceptionHandling/Main.java
@@ -1,22 +1,28 @@
 package ExceptionHandling;
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try{
 
+    public static void main(String[] args) {
+        
+        try(Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter a number: ");
             int num = sc.nextInt();
             System.out.println("You entered: " + num);
-        }
-        catch(ArithmeticException e){
+        } 
+        catch (ArithmeticException e) 
+        {
             System.out.println("You can't divide by Zero");
-        }
-        catch(InputMismatchException e){
+        } 
+        catch (InputMismatchException e) 
+        {
             System.out.println("That wasn't a number!!");
+        } 
+        catch (Exception e)  // Used to catch all the types of exceptions
+        {
+            System.out.println("Something went Wrong!");
+        }
+        finally{ // this always executes
         }
     }
 }
