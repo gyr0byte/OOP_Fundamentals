9b68bcce0076bd02096d2290ec213168709955fe
Gauravlovescode <gqurav69@gmail.com>
2026-01-06 09:28:52 +0545

    Refactor FileWriterr to define file path and content as variables, and update output.txt with additional text

--- FileHandling/FileWriterr.java
+++ FileHandling/FileWriterr.java
@@ -1,11 +1,18 @@
 package FileHandling;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 public class FileWriterr {
     public static void main(String[] args) {
-        try(FileWriter writer = new FileWriter("FileHandling/output.txt")) {
-            writer.write("I like MOMO!");
+        String filePath = "FileHandling/output.txt";
+        String textContent = "I like MOMO!\n It's a great Neplai dish!\n I love it.";
+        try(FileWriter writer = new FileWriter(filePath)) {
+            writer.write(textContent);
+            System.out.println("File has been written");
         } 
+        catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
         catch (IOException e) {
             System.out.println("Couldn't write file!");
         }
