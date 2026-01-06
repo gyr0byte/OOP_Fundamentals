d27ff0202738ace1abd691b1c1f4b237049b7f37
Gauravlovescode <gqurav69@gmail.com>
2026-01-06 09:31:32 +0545

    Update FileWriterr to change text content and modify output.txt with new lines

--- FileHandling/FileWriterr.java
+++ FileHandling/FileWriterr.java
@@ -1,18 +1,27 @@
 package FileHandling;
+
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+
 public class FileWriterr {
+
     public static void main(String[] args) {
         String filePath = "FileHandling/output.txt";
-        String textContent = "I like MOMO!\n It's a great Neplai dish!\n I love it.";
-        try(FileWriter writer = new FileWriter(filePath)) {
+        String textContent = """
+                Ma timro nimti k garum
+                Timi bhitrai harai sakey
+                J gareni man parcha
+                Timilai man parai sakey
+                tunutnuntunnn....
+                """;
+        try (FileWriter writer = new FileWriter(filePath)) {
             writer.write(textContent);
             System.out.println("File has been written");
         } 
-        catch(FileNotFoundException e) {
+        catch (FileNotFoundException e) {
             System.out.println("File not found!");
-        }
+        } 
         catch (IOException e) {
             System.out.println("Couldn't write file!");
         }
