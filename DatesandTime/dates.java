9d344551f29af9d32596f9fe9b4cbcc4db67eb6e
Gauravlovescode <gqurav69@gmail.com>
2026-01-10 21:01:12 +0545

    Refactor dates class to format local date time output

--- DatesandTime/dates.java
+++ DatesandTime/dates.java
@@ -3,15 +3,17 @@ package DatesandTime;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class dates {
     public static void main(String[] args) {
         LocalDate date = LocalDate.now();
         LocalTime time = LocalTime.now();
         LocalDateTime datetime = LocalDateTime.now();
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String newDateTime = datetime.format(formatter);
         System.out.println("Date: " + date);
         System.out.println("Time: " + time);
-        System.out.println("Local date time: " + datetime);
+        System.out.println("Local date time: " + newDateTime);
     }
 }
