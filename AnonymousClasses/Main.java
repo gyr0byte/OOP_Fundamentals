70ff0b9e281313a392269187a5c4cd392c2759a2
Gauravlovescode <gqurav69@gmail.com>
2026-01-10 21:19:32 +0545

    Refactor Main class to demonstrate usage of anonymous classes for custom behavior

--- AnonymousClasses/Main.java
+++ AnonymousClasses/Main.java
@@ -5,8 +5,14 @@ public class Main {
         // Anonymous class = A class that doesn't have a name. Cannot be reused.
         //                   Add custom behavior to a class without having to create a new class.
         //                   Ofter used for one time uses (TimerTask, Runnable, callbacks)
-        Dog dog = new Dog();
-
-        dog.speak();
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog(){
+            @Override
+            void speak(){
+                System.out.println("Malai muji nepali aaucha");
+            }
+        };
+        dog1.speak();
+        dog2.speak();
     }
 }
