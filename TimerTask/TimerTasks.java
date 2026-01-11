79ff4f69f1cda080b1bb9b1bd7917ad34865a44a
Gauravlovescode <gqurav69@gmail.com>
2026-01-11 17:28:51 +0545

    Add TimerTasks class to demonstrate usage of Timer and TimerTask

---
+++ TimerTask/TimerTasks.java
@@ -0,0 +1,17 @@
+package TimerTask;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TimerTasks {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+            @Override
+            public void run(){
+                System.out.println("Hello!");
+            }
+        };
+        timer.schedule(task, 3000); 
+    }
+}
