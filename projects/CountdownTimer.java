5a76f1983ea314af8e01653296e16d8f2d3c4211
Gauravlovescode <gqurav69@gmail.com>
2026-01-11 17:37:29 +0545

    Implement countdown logic in CountdownTimer class

--- projects/CountdownTimer.java
+++ projects/CountdownTimer.java
@@ -5,9 +5,14 @@ public class CountdownTimer {
     public static void main(String[] args) {
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
+            int count = 5;
             @Override
             public void run(){
-                
+                System.out.println(count);
+                count--;
+                if (count < 0) {
+                    System.out.println("Happy Birthday");
+                }
             }
         };
     }
