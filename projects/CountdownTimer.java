2d1a80f614e418bee43e0ae98cdcc364eb7814aa
Gauravlovescode <gqurav69@gmail.com>
2026-01-11 17:42:22 +0545

    Update CountdownTimer to accept user input for countdown duration

--- projects/CountdownTimer.java
+++ projects/CountdownTimer.java
@@ -1,11 +1,16 @@
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class CountdownTimer {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of seconds to countdown from: ");
+        int response = sc.nextInt();
+        
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
-            int count = 5;
+            int count = response;
             @Override
             public void run(){
                 System.out.println(count);
