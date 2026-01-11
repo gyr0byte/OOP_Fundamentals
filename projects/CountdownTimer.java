8c9e710d99c8ab6454393d9b288466b4b49112ed
Gauravlovescode <gqurav69@gmail.com>
2026-01-11 17:35:35 +0545

    Initialize CountdownTimer class with Timer and TimerTask setup

--- projects/CountdownTimer.java
+++ projects/CountdownTimer.java
@@ -1,4 +1,14 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class CountdownTimer {
-    
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run(){
+                
+            }
+        };
+    }
 }
