6341e170802443035518ac1bad6167fe02dba6a5
Gauravlovescode <gqurav69@gmail.com>
2026-01-13 21:07:35 +0545

    Implement Product class with generics and update Main class to demonstrate usage

--- Generics/Main.java
+++ Generics/Main.java
@@ -3,6 +3,11 @@ package Generics;
 public class Main {
     public static void main(String[] args) {
         Box<String> box = new Box<>();
-        
+        box.setItem("Banana");
+        System.out.println(box.getItem());
+
+        Product<String, Double> product = new Product<>("apple", 0.50);
+        System.out.println(product.getItem());
+        System.out.println(product.getPrice());
     }
 }
---
+++ Generics/Product.java
@@ -0,0 +1,16 @@
+package Generics;
+
+public class Product<T, U> {
+    T item;
+    U price;
+    public Product(T item, U price) {
+        this.item = item;
+        this.price = price;
+    }
+    public T getItem(){
+        return this.item;
+    }
+    public U getPrice(){
+        return this.price;
+    }
+}
