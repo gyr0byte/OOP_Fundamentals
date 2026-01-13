5b82cdd37c39872125d654369c44bd7aa0993b27
Gauravlovescode <gqurav69@gmail.com>
2026-01-13 21:10:50 +0545

    Refactor Main class to instantiate multiple Product objects and display their details

--- Generics/Main.java
+++ Generics/Main.java
@@ -6,8 +6,11 @@ public class Main {
         box.setItem("Banana");
         System.out.println(box.getItem());
 
-        Product<String, Double> product = new Product<>("apple", 0.50);
-        System.out.println(product.getItem());
-        System.out.println(product.getPrice());
+        Product<String, Double> product1 = new Product<>("apple", 0.50);
+        Product<String, Integer> product2 = new Product<>("Ticket", 50);
+        System.out.println(product1.getItem());
+        System.out.println(product1.getPrice());
+        System.out.println(product2.getItem());
+        System.out.println(product2.getPrice());
     }
 }
