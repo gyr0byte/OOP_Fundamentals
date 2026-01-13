44fad54406c37f82f3c163aafb1646fb1e6117a4
Gauravlovescode <gqurav69@gmail.com>
2026-01-13 20:59:57 +0545

    Refactor Box class to use generics and update Main class to instantiate Box with a String type

--- Generics/Box.java
+++ Generics/Box.java
@@ -1,5 +1,14 @@
 package Generics;
 
-public class Box {
+public class Box<T> {
+    T item;
+
+    public void setItem(T item){
+        this.item = item;
+    }
+
+    public T getItem(){
+        return this.item;
+    }
     
 }
--- Generics/Main.java
+++ Generics/Main.java
@@ -2,6 +2,7 @@ package Generics;
 
 public class Main {
     public static void main(String[] args) {
+        Box<String> box = new Box<>();
         
     }
 }
