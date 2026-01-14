56373f2c04ce02aa510595b6c5d0a3868d806adc
Gauravlovescode <gqurav69@gmail.com>
2026-01-14 21:27:40 +0545

    Update Main class to add coconut price and remove apple from HashMap

--- HashMap/Main.java
+++ HashMap/Main.java
@@ -8,6 +8,8 @@ public class Main {
         map.put("apple", 0.50);
         map.put("orange", 0.75);
         map.put("banana", 0.25);
+        map.put("coconut", 1.00);
+        map.remove("apple");
         System.out.println(map);
     }
 }
