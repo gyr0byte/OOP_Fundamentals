2684f438eae622be213e1126e75ee10a8fd5351e
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 16:44:20 +0545

    Refactor Main class to improve output formatting and structure

--- MultiThreading/Main.java
+++ MultiThreading/Main.java
@@ -4,7 +4,18 @@ public class Main {
     public static void main(String[] args) {
         Thread thread1 = new Thread(new MultiRunnable("Ping"));
         Thread thread2 = new Thread(new MultiRunnable("Pong"));
+
+        System.out.println("Game Start");
         thread1.start();
         thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } 
+        catch (InterruptedException ex) {
+            System.out.println("Main thread was interrupted!");
+        }
+        System.out.println("Game Over!!");
     }
 }
