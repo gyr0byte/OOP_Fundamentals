b0c762d6caf3507b2acc3becc7d148ede84d37f5
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 16:40:41 +0545

    Add constructor to MultiRunnable for initializing text parameter

--- MultiThreading/MultiRunnable.java
+++ MultiThreading/MultiRunnable.java
@@ -1,6 +1,11 @@
 package MultiThreading;
 
 public class MultiRunnable implements Runnable {
+    private final String text;
+    
+    MultiRunnable(String text) {
+        this.text = text;
+    }
     @Override
     public void run() {
         for (int i = 1; i <= 5; i++) {
