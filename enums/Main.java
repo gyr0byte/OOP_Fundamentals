7a0f63a4d9297c440064f96b5ffdfcee9d3aedc0
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 15:04:04 +0545

    Refactor Main class to improve day classification output and clean up commented code

--- enums/Main.java
+++ enums/Main.java
@@ -1,8 +1,18 @@
 package enums;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Day day =  Day.SUNDAY;
-        System.out.println(day);
+        // System.out.println(day);
+        // System.out.println(day.getDayNumber());
+
+        switch (day) {
+            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> System.out.println("It is a weekday");
+            case SATURDAY, SUNDAY -> System.out.println("It is a weekend");
+        }
+        sc.close();
     }
 }
