9603fe1bc44cda14bc43140a0d1e6be027415347
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 16:00:23 +0545

    Refactor Main class to start a thread for countdown and remove manual sleep loop

---
+++ Threading/MyRunnable.java
@@ -0,0 +1,15 @@
+package Threading;
+
+public class MyRunnable implements Runnable{
+    @Override
+    public void run(){
+         for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } 
+            catch (InterruptedException e) {
+                System.out.println("Thread was interrupted");
+            }
+        }
+    }
+}
