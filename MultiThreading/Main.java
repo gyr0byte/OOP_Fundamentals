19fe6829cabe8da992cc822fcde5fc96934185b4
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 16:42:00 +0545

    Update MultiRunnable to accept a text parameter and modify Main to use it for thread output

--- MultiThreading/Main.java
+++ MultiThreading/Main.java
@@ -2,8 +2,8 @@ package MultiThreading;
 
 public class Main {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new MultiRunnable());
-        Thread thread2 = new Thread(new MultiRunnable());
+        Thread thread1 = new Thread(new MultiRunnable("Ping"));
+        Thread thread2 = new Thread(new MultiRunnable("Pong"));
         thread1.start();
         thread2.start();
     }
--- MultiThreading/MultiRunnable.java
+++ MultiThreading/MultiRunnable.java
@@ -11,7 +11,7 @@ public class MultiRunnable implements Runnable {
         for (int i = 1; i <= 5; i++) {
             try {
                 Thread.sleep(1000);
-                System.out.println(Thread.currentThread().getName() + " " + i);
+                System.out.println(text);
             } 
             catch (InterruptedException e) {
                 System.out.println("Thread was interrupted");
