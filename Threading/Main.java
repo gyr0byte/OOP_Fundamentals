fd8949a505add5351eacde48ec6be556a38c4624
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 15:51:27 +0545

    Add Main class with a basic user input greeting functionality

---
+++ Threading/Main.java
@@ -0,0 +1,15 @@
+package Threading;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("You have 5 seconds to enter your name");
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+
+        sc.close();
+    }
+}
