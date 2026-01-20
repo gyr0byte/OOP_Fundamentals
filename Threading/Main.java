ffa65cd4e57dc662c19b17978ef2a7a2334c94c9
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 15:53:22 +0545

    Add countdown before user input for name

--- Threading/Main.java
+++ Threading/Main.java
@@ -6,6 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("You have 5 seconds to enter your name");
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } 
+            catch (InterruptedException e) {
+            }
+        }
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
         System.out.println("Hello " + name);
