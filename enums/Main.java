1d9b36e6a04bf09126f92b2761366ca2a0a01c8a
Gauravlovescode <gqurav69@gmail.com>
2026-01-20 15:06:55 +0545

    Refactor main method to prompt user for day input and handle exceptions

--- enums/Main.java
+++ enums/Main.java
@@ -5,14 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Day day =  Day.SUNDAY;
-        // System.out.println(day);
-        // System.out.println(day.getDayNumber());
+        System.out.println("Enter a day of the week");
+        String response = sc.nextLine().toUpperCase();
+        try {
+            Day day = Day.valueOf(response);
+            // System.out.println(day);
+            // System.out.println(day.getDayNumber());
 
-        switch (day) {
-            case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> System.out.println("It is a weekday");
-            case SATURDAY, SUNDAY -> System.out.println("It is a weekend");
+            switch (day) {
+                case MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> System.out.println("It is a weekday");
+                case SATURDAY, SUNDAY -> System.out.println("It is a weekend");
+            }
+        } 
+        catch (IllegalArgumentException e) {
+            System.out.println("Please enter a valid day");
         }
+
         sc.close();
     }
 }
